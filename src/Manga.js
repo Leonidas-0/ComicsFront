ed3a133e5f65cc77d2efdb2331b3466361e6e9ff
Leonidas-0 <72760800+Leonidas-0@users.noreply.github.com>
2023-05-18 15:16:31 +0400

    changed jpg to png

--- src/Manga.js
+++ src/Manga.js
@@ -14,7 +14,6 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import solo from './sololeveling.jpg';
 import { Link } from 'react-router-dom';
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
